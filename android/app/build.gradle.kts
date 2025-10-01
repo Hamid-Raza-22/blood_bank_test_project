c6e8dff28dafc852587e5c18c0ec9d14a8f135be
Hamid Raza <hamidraza.engr@gmail.com>
2025-10-01 14:51:55 +0500

    feat: Update SDK versions and enable core library desugaring
    - Enabled `isCoreLibraryDesugaringEnabled` in `android/app/build.gradle.kts`.
    - Updated `minSdk` to `flutter.minSdkVersion` in `android/app/build.gradle.kts`.
    - Updated Dart SDK constraint from `>=3.9.0 <4.0.0` to `>=3.9.2 <4.0.0` in `pubspec.lock`.
    - Updated Dart SDK constraint from `^3.8.1` to `^3.9.2` in `pubspec.yaml`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -24,7 +25,7 @@ android {
         applicationId = "com.example.blood_bank_test_project"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
